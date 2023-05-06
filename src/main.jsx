d61731c9f165a5601360d2d9af124c0a333bd9a2
Land Unexplored <landunexplored@gmail.com>
2023-05-06 10:09:09 -0400

    plug in provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import "./index.css";
+import { createStore } from "redux";
+import { counter } from "./reducers/counter.js";
+import { Provider } from "react-redux";
+
+const store = createStore(counter);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
