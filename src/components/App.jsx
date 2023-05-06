827bffe7cfea67d9fc27912a4f49edeadeb70d99
Land Unexplored <landunexplored@gmail.com>
2023-05-06 10:11:33 -0400

    create counter component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <></>;
+import { connect } from "react-redux";
+import { increment, decrement } from "../actions";
+
+const mapStateToProps = (state) => {
+  return {
+    count: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+  };
+};
+
+function App({ count, increment, decrement }) {
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
 
-export default App;
+const CounterComponent = connect(mapStateToProps, mapDispatchToProps)(App);
+export default CounterComponent;
